03e57679e2aa40cfa3f679e9a55f57abcf6a2227
yashshekhar01 <yashshekhar2934@gmail.com>
2022-04-26 03:45:53 +0530

    Socket.io Events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,9 @@
-io()
+const socket = io()
+
+socket.on('countUpdated', (count) => {
+    console.log('The count has been updated!', count)
+})
+
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked')
+})
